ce6ce1a6cd73874090a5a78f54112a04df7544bd
github-himanshu-yadav <himanshuyadav20@hotmail.com>
2023-03-06 10:21:43 +0530

    News

---
+++ script.js
@@ -0,0 +1,202 @@
+const data =  [
+   {
+       "source": {
+           "id": "bbc-news",
+           "name": "BBC News"
+       },
+       "author": "https://www.facebook.com/bbcnews",
+       "title": "India paper leaks: Cheating plagues India jobs coveted by millions",
+       "description": "Cheating in exams is common in India, where students go to great lengths to land a government job.",
+       "url": "https://www.bbc.co.uk/news/world-asia-india-64495673",
+       "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/6851/production/_128550762_gettyimages-940585086-594x594.jpg",
+       "publishedAt": "2023-02-08T00:25:53Z",
+       "content": "On a chilly December morning, police in the western state of Rajasthan saw a bus heading towards Udaipur city and followed it. \r\nThe night before, they had received a tip-off - the question paper for… [+6810 chars]",
+   },
+   {
+       "source": {
+           "id": "bbc-news",
+           "name": "BBC News"
+       },
+       "author": "https://www.facebook.com/bbcnews",
+       "title": "G20 meeting deadlocked over calling out Ukraine war",
+       "description": "Russia and China refused to condemn Moscow's invasion at a meeting of finance ministers in India.",
+       "url": "https://www.bbc.co.uk/news/world-asia-64773618",
+       "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/15A3B/production/_128753688_ee1be69fb8a9e8e62545fb2dd88b306eb5a2931c0_0_5006_33381000x667.jpg",
+       "publishedAt": "2023-02-25T18:51:30Z",
+       "content": "Finance ministers of the world's largest economies have failed to agree on a closing statement following talks in India due to differences over the war in Ukraine. \r\nRussia and China refused to conde… [+2139 chars]"
+   },
+   {
+       "source": {
+           "id": "bbc-news",
+           "name": "BBC News"
+       },
+       "author": "https://www.facebook.com/bbcnews",
+       "title": "Why India can't keep its domestic helpers safe",
+       "description": "The case of a teenage maid who was allegedly tortured highlights the plight of domestic helpers.",
+       "url": "https://www.bbc.co.uk/news/world-asia-india-64594779",
+       "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/E833/production/_128634495_whatsappimage2023-02-09at3.24.36pm.jpg",
+       "publishedAt": "2023-02-17T00:02:37Z",
+       "content": "Indian police and social workers rescued a battered and bruised 14-year-old last week from the home in a Delhi suburb where she worked as a domestic helper. Experts say such workers are vulnerable to… [+7301 chars]"
+   },
+   {
+       "source": {
+           "id": "bbc-news",
+           "name": "BBC News"
+       },
+       "author": "https://www.facebook.com/bbcnews",
+       "title": "Padayappa: The friendly India elephant whose fame is a curse",
+       "description": "Padayappa has long been a fan favourite in India's Kerala state. So why is he controversial now?",
+       "url": "https://www.bbc.co.uk/news/world-asia-india-64480308",
+       "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/16E7A/production/_128481839_padayappaphotobyhadleerenjithimg_2081.jpg",
+       "publishedAt": "2023-02-04T02:49:19Z",
+       "content": "Padayappa is an iconic figure in the southern Indian state of Kerala. \r\nThe wild elephant, who lives in the state's lush jungles, is known for his flappy ears, massive tusks and friendly ways. \r\nPada… [+6019 chars]"
+   },
+   {
+       "source": {
+           "id": "bbc-news",
+           "name": "BBC News"
+       },
+       "author": "https://www.facebook.com/bbcnews",
+       "title": "Assam: India child brides desperate after mass arrests",
+       "description": "Women in Assam are protesting after thousands of men were detained in child marriage cases.",
+       "url": "https://www.bbc.co.uk/news/world-asia-india-64564861",
+       "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/7BA9/production/_128575613_gettyimages-1246780415-594x594.jpg",
+       "publishedAt": "2023-02-10T10:44:19Z",
+       "content": "Momina Khatun is convinced she is cursed.\r\nShe's one of hundreds of women in the north-eastern Indian state of Assam who married under the age of 18 - and are now stuck in limbo after their husbands … [+8245 chars]"
+   },
+   {
+       "source": {
+           "id": "bbc-news",
+           "name": "BBC News"
+       },
+       "author": null,
+       "title": "Women's T20 World Cup: India stroll to comfortable six-wicket win over West Indies",
+       "description": "India beat West Indies comfortably by six wickets to remain unbeaten in the Women's T20 World Cup in South Africa.",
+       "url": "https://www.bbc.co.uk/sport/cricket/64653368",
+       "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/019E/production/_128641400_gettyimages-1247158415.jpg",
+       "publishedAt": "2023-02-15T16:23:20Z",
+       "content": "<table><tr><th>Women's T20 World Cup, Group 2: West Indies v India</th></tr>\r\n<tr><td>West Indies 118-6 (20 overs): Taylor 42 (40); Sharma 3-15</td></tr><tr><td>India 119-4 (20 overs): Ghosh 44* (32)… [+1147 chars]"
+   },
+   {
+       "source": {
+           "id": "bbc-news",
+           "name": "BBC News"
+       },
+       "author": "https://www.facebook.com/bbcnews",
+       "title": "A dozen South African cheetahs arrive in India",
+       "description": "Twelve cheetahs from South Africa have been flown to India on Saturday.",
+       "url": "https://www.bbc.co.uk/news/world-asia-india-64690210",
+       "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/D563/production/_128672645_fadb501f-114c-49f8-81c7-4cf2036fea6b.jpg",
+       "publishedAt": "2023-02-18T13:36:03Z",
+       "content": "Twelve cheetahs from South Africa have been flown to India as part of an agreement to introduce dozens of the mammals there over the next decade.\r\nAsiatic cheetahs became extinct in India in the late… [+1242 chars]"
+   },
+   {
+       "source": {
+           "id": "bbc-news",
+           "name": "BBC News"
+       },
+       "author": "https://www.facebook.com/bbcnews",
+       "title": "Toyah Cordingley: Australia flies man from India over beach murder",
+       "description": "Rajwinder Singh is accused of murdering Toyah Cordingley, found dead in Queensland in 2018.",
+       "url": "https://www.bbc.co.uk/news/world-australia-64803339",
+       "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/7BEE/production/_127562713_toyah.jpg",
+       "publishedAt": "2023-02-28T16:31:28Z",
+       "content": "Australian police are extraditing a man from India over the murder of a young woman found dead on a Queensland tropical beach in 2018.\r\nRajwinder Singh is being flown from Delhi to Melbourne accompan… [+1886 chars]"
+   },
+   {
+       "source": {
+           "id": "bbc-news",
+           "name": "BBC News"
+       },
+       "author": "https://www.facebook.com/bbcnews",
+       "title": "BBC India: Director-general tells staff to report without fear",
+       "description": "Tim Davie says nothing is more important than reporting impartially, after officials search BBC offices.",
+       "url": "https://www.bbc.co.uk/news/world-asia-india-64747641",
+       "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/2E42/production/_128724811_mediaitem128724810.jpg",
+       "publishedAt": "2023-02-23T14:37:38Z",
+       "content": "The BBC will not be put off from reporting without fear or favour, its Director-General Tim Davie has said in an email to staff in India.\r\nIt follows searches at BBC offices in Delhi and Mumbai by ta… [+1679 chars]"
+   },
+   {
+       "source": {
+           "id": "wired",
+           "name": "Wired"
+       },
+       "author": "Yashraj Sharma",
+       "title": "The Mountain Village in the Path of India’s Electric Dreams",
+       "description": "The country has discovered enough lithium to electrify every vehicle on its roads, but the massive deposit has tensions running high.",
+       "url": "https://www.wired.com/story/lithium-village-in-the-path-of-indias-electric-dreams/",
+       "urlToImage": "https://media.wired.com/photos/63feeaab58853a794e686a50/191:100/w_1280,c_limit/Mountain-Village-In-Path-Of-India's-Electric-Dreams-Business-1235702792.jpg",
+       "publishedAt": "2023-03-01T12:00:00Z",
+       "content": "The government is really obsessed with electrifying India. And thats rooted in its net-zero targets. If you see the direction, thats very, very clear, he says. Every private investor and company is s… [+3168 chars]"
+   },
+   {
+       "source": {
+           "id": "reuters",
+           "name": "Reuters"
+       },
+       "author": null,
+       "title": "Analysis: Adani's adversity raises the stakes for India and investors - Reuters India",
+       "description": "As Indian tycoon <a href=\"/world/india/gautam-adani-one-indias-most-powerful-men-suffers-rare-defeat-2023-02-01/\">Gautam Adani's</a> woes deepen and force him to drop a share sale, foreign investors and Indian regulators are abandoning any pretence that the c…",
+       "url": "https://www.reuters.com/world/india/adanis-adversity-raises-stakes-india-investors-2023-02-02/",
+       "urlToImage": "https://www.reuters.com/resizer/hEn0DOx_YfDcaXggCSab4jY1K54=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/NQG6HV4W7ZNVXK7OUVJ6KOMRI4.jpg",
+       "publishedAt": "2023-02-02T09:15:00Z",
+       "content": "SINGAPORE, Feb 2 (Reuters) - As Indian tycoon Gautam Adani's woes deepen and force him to drop a share sale, foreign investors and Indian regulators are abandoning any pretence that the conglomerate'… [+4632 chars]"
+   },
+   {
+       "source": {
+           "id": "reuters",
+           "name": "Reuters"
+       },
+       "author": null,
+       "title": "India rupee expected to fare better in 2023 – chief economic adviser - Reuters India",
+       "description": "The Indian rupee is expected to be under far less pressure than it was in 2022, as a global economic slowdown will mean a potentially weaker dollar and lower commodity prices which would lead to lower imports, the country's chief economic adviser said on Thur…",
+       "url": "https://www.reuters.com/world/india/india-rupee-expected-fare-better-2023-chief-economic-adviser-2023-02-02/",
+       "urlToImage": "https://www.reuters.com/resizer/vRP7q83rBTN4pUnB_mbvyXrm3jA=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/GKP55GQLQBPYJMVJEMROZT4JHY.jpg",
+       "publishedAt": "2023-02-02T14:27:00Z",
+       "content": "NEW DELHI, Feb 2 (Reuters) - The Indian rupee is expected to be under far less pressure than it was in 2022, as a global economic slowdown will mean a potentially weaker dollar and lower commodity pr… [+1675 chars]"
+   },
+   {
+       "source": {
+           "id": "reuters",
+           "name": "Reuters"
+       },
+       "author": null,
+       "title": "S India ramps up spending in last budget ahead of election - Reuters India",
+       "description": "India's government on Wednesday unveiled a $550 billion budget for the next fiscal year that starts on April 1, with a plan for record capital spending while reining in the fiscal deficit.",
+       "url": "https://www.reuters.com/world/india/s-india-ramps-up-spending-last-budget-ahead-election-2023-02-02/",
+       "urlToImage": "https://www.reuters.com/resizer/DQJbnQS3J_Tac9mIC4iM6_wB3uY=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/54KGIHIR5ROE3HJEUC7U7PMI4A.jpg",
+       "publishedAt": "2023-02-02T12:32:00Z",
+       "content": "NEW DELHI, Feb 2 (Reuters) - India's government on Wednesday unveiled a $550 billion budget for the next fiscal year that starts on April 1, with a plan for record capital spending while reining in t… [+2292 chars]"
+   },
+   {
+       "source": {
+           "id": "reuters",
+           "name": "Reuters"
+       },
+       "author": null,
+       "title": "India 'fairly' confident of cutting fiscal deficit to 4.5% of GDP in 3 ... - Reuters India",
+       "description": "India is 'fairly' confident it can meet its target to cut its fiscal deficit by nearly 200 basis points to 4.5% of GDP in the next three years, assuming there is no major global economic shock, a top government official told Reuters on Thursday.",
+       "url": "https://www.reuters.com/world/india/india-fairly-confident-cutting-fiscal-deficit-45-gdp-3-years-official-2023-02-02/",
+       "urlToImage": "https://www.reuters.com/resizer/j_CkiWE7A5fHF4D-GIHdu5pBFbY=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/VH5MAJGMG5JDXKRMALQOIPNWQU.jpg",
+       "publishedAt": "2023-02-02T16:00:00Z",
+       "content": "NEW DELHI, Feb 2 (Reuters) - India is 'fairly' confident it can meet its target to cut its fiscal deficit by nearly 200 basis points to 4.5% of GDP in the next three years, assuming there is no major… [+1604 chars]"
+   }
+]
+const newsContainer = document.querySelector(".news-container");
+console.log(newsContainer)
+
+data.forEach((item, idx)=>{
+   const itemDiv = document.createElement("div");
+   itemDiv.innerHTML = `
+   <div class="news-item" key=${idx}>
+      <div class = "news-subcontainer">
+         <p class = "news-index">News : ${idx + 1}</p>
+         <p class = "news-title">${item.title}</p>
+         <p class = "news-source">Source: ${item.source.name}</p>
+         <img class="news-image" src=${item.urlToImage} alt = "${item.content}" />
+         <p class = "news-desc">${item.description} Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam asperiores, quos magnam quaerat laborum odio optio saepe delectus quae adipisci explicabo eveniet consequuntur aut, laboriosam perspiciatis voluptatem nihil incidunt neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, earum. Assumenda vero, at quia repudiandae porro obcaecati amet fugiat, facere magni dolorem sit ratione repellat impedit molestias quasi delectus repellendus?</p>
+         <a href=${item.url} target="_blank"><p class = "read-more">Read more</p></a>
+      </div>
+   </div>`;
+   newsContainer.append(itemDiv)
+})
